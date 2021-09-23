0375e1e8c91c56fe5ffdd0019ba25c4d68cf192c
terryleotuba <terryleotuba@gmail.com>
2021-09-23 20:49:31 +0900

    価格半角数制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :shipping_date
   
-  validates :goods_name, :goods_description, :price, presence: true
-  validates :image,                                  presence: true
+  validates :goods_name, :goods_description, presence: true
+  validates :image,                          presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   validates :goods_category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :goods_status_id, numericality: { other_than: 1 , message: "can't be blank"}
