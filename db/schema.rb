575723dbac0bb40dbc9bff007186c7fd8246f6cc
terryleotuba <terryleotuba@gmail.com>
2021-09-23 17:01:20 +0900

    itemモデルリセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_042855) do
+ActiveRecord::Schema.define(version: 2021_09_23_073027) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
