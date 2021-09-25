615e64d7f7e54595af89499537d82dcc44c41dbb
terryleotuba <terryleotuba@gmail.com>
2021-09-25 16:36:10 +0900

    コントローラに制限追記及びビューファイルを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -40,4 +41,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:goods_name, :goods_description, :price, :goods_category_id, :goods_status_id, :prefecture_id,
                                  :shipping_cost_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    # 商品が売れている条件を商品購入機能実装後に追加する予定
+    unless user_signed_in? && current_user.id == item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
