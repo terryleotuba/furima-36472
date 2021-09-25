dc47e795f037eb5797243997445575ce8c4bb677
terryleotuba <terryleotuba@gmail.com>
2021-09-25 16:37:03 +0900

    Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,9 +45,6 @@ class ItemsController < ApplicationController
   def move_to_index
     item = Item.find(params[:id])
     # 商品が売れている条件を商品購入機能実装後に追加する予定
-    unless user_signed_in? && current_user.id == item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == item.user_id
   end
-
 end
